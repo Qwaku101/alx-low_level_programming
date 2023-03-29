d268ee81bdae4443b9fcaca020b6877ed980d9d6
Qwaku101 <ubuntu403@gmail.com>
2023-03-29 14:20:06 -0700

    updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,18 +2,19 @@
 
 /**
 *print_most_numbers - prints
-* owned by Bwave ICT
+*
 *Return: void
 */
 
 void print_most_numbers(void)
-{
-	int c;
 
-	for (c = '0'; c <= '9'; c++)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+		if (i != 2 && i != 4)
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
