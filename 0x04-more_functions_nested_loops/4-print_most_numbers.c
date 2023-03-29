7e6f8e0ba40be56cf5404e62f41fc8a571043703
Qwaku101 <ubuntu403@gmail.com>
2023-03-29 13:34:41 -0700

    updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,9 @@
 #include "main.h"
+
 /**
- * print_most_numbers - print numbers 0-9 excluding 2 and 4
- *
- * Return: void
- *
- */
+*print_most_numbers - prints
+*Return: void
+*/
 
 void print_most_numbers(void)
 {
@@ -12,8 +11,8 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
 	_putchar('\n');
 }
